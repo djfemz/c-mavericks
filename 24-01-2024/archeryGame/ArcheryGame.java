e1cccf86188795dc5da5dba07c90bb32116da96d
Orisha <orisakwenwokocha1@gmail.com>
2024-02-21 23:02:39 +0100

    added final score board

--- 24-01-2024/archeryGame/ArcheryGame.java
+++ 24-01-2024/archeryGame/ArcheryGame.java
@@ -104,6 +104,33 @@ public class ArcheryGame{
 		return score;
 	}
 
+	public String finalScoreBoard(){
+		String horizontal = "=".repeat(41);
+		String horizontal2 = "-".repeat(41);
+		String vertical = "|";
+		String blank = " ";
+		String column = "%s\n%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s";
+		String row = "%s\n%s%s%4d%4s%s%s%4d%4s%s%s%4d%4s%s%s%4d%4s%s";
+
+		String boardRow = String.format(column, horizontal, vertical, blank, "Player1", blank, vertical, blank,
+				"Player2", blank, vertical, blank, "Player3", blank, vertical, blank, "Player4", blank, vertical);
+
+		String firstRow = String.format(row, horizontal, vertical, blank, scoreBoard[0][0], blank, vertical, blank,
+				scoreBoard[1][0], blank, vertical, blank, scoreBoard[2][0], blank, vertical, blank, scoreBoard[3][0],
+				blank, vertical);
+		String secondRow = String.format(row, horizontal2, vertical, blank, scoreBoard[0][1], blank, vertical, blank,
+				scoreBoard[1][1], blank, vertical, blank, scoreBoard[2][1], blank, vertical, blank, scoreBoard[3][1],
+				blank, vertical);
+		String thirdRow = String.format(row, horizontal2, vertical, blank, scoreBoard[0][2], blank, vertical, blank,
+				scoreBoard[1][2], blank, vertical, blank, scoreBoard[2][2], blank, vertical, blank, scoreBoard[3][2],
+				blank, vertical);
+		String total = String.format(row, horizontal, vertical, blank, totalOfScores[0], blank, vertical, blank,
+				totalOfScores[1], blank, vertical, blank, totalOfScores[2], blank, vertical, blank, totalOfScores[3],
+				blank, vertical);
+
+		return String.format("%s%n%s%n%s%n%s%n%s%n%s%n", boardRow, firstRow, secondRow, thirdRow, totalRow, horizontal);
+	}
+
 
 
 
